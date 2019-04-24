7cec7fa6d5d3a4131c23083e2db3b33ae4a96303
Kerrie O'Leary <KerrieOLeary@Kerries-MacBook-Pro.local>
2019-04-24 20:19:46 +0100

    some stuff

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
 echo "Hello World. This is Version 2";
+echo "<p>Password is " .getenv('DB_PASSWORD');*/
 
-echo "<p>Password is " .getenv('DB_PASSWORD');
+class DBikes {
+    public $number;
+    public $name;
+}
+
+$listOfBikes = [];
+
+for($i=0; $i < 10; $i++){
+
+    $bike = new DBike;
+    $bike->number=$i;
+    $bike->name="Name $i";
+
+    $listOfBikes[] = $bike;
+}
+
+//print_r($listOfBikes);
+
+header('Content-Type: application/json');
+$jsonOutput = json_encode($listOfBikes);
+
+//Output the JSON data
+echo $jsonOutput;
